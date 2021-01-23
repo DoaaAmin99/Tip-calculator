4852653ad4e1942926a4cb86971928291068cdd0
DoaaAmin99 <dondon2799@gmail.com>
2021-01-23 23:18:59 +0200

    OOP

--- js/tip.js
+++ js/tip.js
@@ -1,32 +1,66 @@
 
-//Calculation function
-function calculation() {
-  //declaration
-  var mybill = document.getElementById("mybill").value;
-  var serviceQuality = document.getElementById("ServiceQuality").value;
-  var people = document.getElementById("people").value;
+// //Calculation function
+// function calculation() {
+//   //declaration
+//   var mybill = document.getElementById("mybill").value;
+//   var serviceQuality = document.getElementById("ServiceQuality").value;
+//   var people = document.getElementById("people").value;
   
 
-  //validate bill amount
-  if (mybill === "" || serviceQuality === 0 || mybill<=0) {
-      alert("Please enter correct values");
+//   //validate bill amount
+//   if (mybill === "" || serviceQuality === 0 || mybill<=0) {
+//       alert("Please enter correct values");
       
-    };
+//     };
 
-  //number of people
-  if (people === "" || people <= 0) {
+//   //number of people
+//   if (people === "" || people <= 0) {
+//       alert("please enter NUMBER OF PEOPLE")
+//       return;
+      
+//     }
+  
+  
+//   //total tip
+//   var total = (mybill / people ) *serviceQuality ;
+//   document.getElementById("result").innerHTML = total;
+// }
+
+// //calling the Calculation function
+// document.getElementById("claculate").onclick = function() {
+//   calculation();
+// };
+
+//Using OOP
+
+var mybill = document.getElementById("mybill");
+var serviceQuality = document.getElementById("ServiceQuality");
+var people = document.getElementById("people");
+class Tip{
+  //constructor function
+  constructor(mybill,serviceQuality,people){
+    this.mybill=mybill;
+    this.serviceQuality=serviceQuality;
+    this.people=people;
+  }
+  //calculation function
+  calculation(){
+    //validate bill amount
+    if(this.mybill === "" || this.mybill<=0){
+      alert("Please enter correct values");
+    };
+    //number of people
+    if(this.people === "" || this.people<=0){
       alert("please enter NUMBER OF PEOPLE")
       return;
-      
-    }
-  
-  
-  //total tip
-  var total = (mybill / people ) *serviceQuality ;
-  document.getElementById("result").innerHTML = total;
-}
+    };
+    //  total tip
+    var total=((this.mybill/this.people)*this.serviceQuality)
+    document.getElementById("result").innerHTML=total;
 
-//calling the Calculation function
+  }
+}
+var tip = new Tip(mybill.value,serviceQuality.value,people.vlaue);
 document.getElementById("claculate").onclick = function() {
-  calculation();
-};
+   tip.calculation();
+  };
